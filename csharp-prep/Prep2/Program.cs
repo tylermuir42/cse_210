da57123042ff1a58af2db5ac22e49805120038f9
Tyler Muir <tylermuir42@gmail.com>
2025-10-29 13:23:20 -0600

    Completed Prep 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+
+        int grade = int.Parse(userInput);
+        string letter = "";
+        bool pass = false;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+            pass = true;
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+            pass = true;
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+            pass = true;
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+            pass = false;
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+            pass = false;
+        }
+        Console.WriteLine($"Grade: {letter}");
+        if (pass)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed! Try again");
+        }
+        
     }
 }
