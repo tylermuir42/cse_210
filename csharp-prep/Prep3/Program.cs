38a3109c7d61d34c4157cff9240d364a60769881
Tyler Muir <tylermuir42@gmail.com>
2025-10-30 09:45:48 -0600

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        //Console.WriteLine(number);
+
+        bool response = false;
+        string guess;
+
+        int count = 0;
+
+        while (response == false)
+        {
+            Console.Write("Guess my number: ");
+            guess = Console.ReadLine();
+            int guessNum = int.Parse(guess);
+
+            if (guessNum == number)
+            {
+                Console.Write("That's right!");
+                response = true;
+            }
+            else if (guessNum > number)
+            {
+                Console.WriteLine("Lower!");
+                count++;
+            }
+            else if (guessNum < number)
+            {
+                Console.WriteLine("Higher!");
+                count++;
+            }
+        }
+        Console.WriteLine($" It took you {count} attempts");
     }
 }
