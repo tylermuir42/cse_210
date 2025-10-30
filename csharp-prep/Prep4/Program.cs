7e7206f11b39e3a38b0d9e586d5ad2dc22876249
Tyler Muir <tylermuir42@gmail.com>
2025-10-30 11:39:43 -0600

    Completed Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        bool end = false;
+        string numStr;
+        int count = 0;
+        float total = 0;
+
+        while (end == false)
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished: ");
+            numStr = Console.ReadLine();
+            int number = int.Parse(numStr);
+
+            numbers.Add(number);
+            count++;
+
+            if (number == 0)
+            {
+                end = true;
+            }
+        }
+
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+        Console.WriteLine($"The sum is :{total.ToString()}");
+
+        float average = total / (count - 1);
+        Console.WriteLine($"The average is: {average.ToString()}");
+
+        numbers.Sort();
+        int largest = numbers[numbers.Count - 1];
+        Console.WriteLine($"The largest number in the list is {largest}.");
+
+        Console.WriteLine("Your sorted list: ");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
