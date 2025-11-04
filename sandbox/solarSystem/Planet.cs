64fca1e873507ca69304e8492cc20557bab81a13
Tyler Muir <tylermuir42@gmail.com>
2025-11-04 09:56:14 -0700

    Created solar system and planets

---
+++ sandbox/solarSystem/Planet.cs
@@ -0,0 +1,9 @@
+class Planet
+{
+    public string _name = "";
+    public double _diameter;
+    public void DisplayPlanetInformation()
+    {
+        Console.WriteLine($"The planet name: {_name}, diameter: {_diameter}");
+    }
+}
--- sandbox/solarSystem/Program.cs
+++ sandbox/solarSystem/Program.cs
@@ -2,6 +2,15 @@
 {
     static void Main()
     {
-        Console.WriteLine("Bonjour tout le meonde");
+        //Console.WriteLine("Bonjour tout le meonde");
+        Planet mercury = new Planet();
+        mercury._name = "Mercury";
+        mercury._diameter = 1234.343;
+        mercury.DisplayPlanetInformation();
+
+        Planet venus = new Planet();
+        venus._name = "Venus";
+        venus._diameter = 234.568;
+        venus.DisplayPlanetInformation();
     }
 }
