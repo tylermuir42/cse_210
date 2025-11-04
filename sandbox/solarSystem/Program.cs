4919318e6292d06e92c9771ee026ac201d372bd6
Tyler Muir <tylermuir42@gmail.com>
2025-11-04 10:02:22 -0700

    Finish solar system

--- sandbox/solarSystem/Program.cs
+++ sandbox/solarSystem/Program.cs
@@ -6,11 +6,16 @@
         Planet mercury = new Planet();
         mercury._name = "Mercury";
         mercury._diameter = 1234.343;
-        mercury.DisplayPlanetInformation();
+        //mercury.DisplayPlanetInformation();
 
         Planet venus = new Planet();
         venus._name = "Venus";
         venus._diameter = 234.568;
-        venus.DisplayPlanetInformation();
+        //venus.DisplayPlanetInformation();
+
+        SolarSystem solarSystem = new SolarSystem();
+        solarSystem._solarSystem.Add(mercury);
+        solarSystem._solarSystem.Add(venus);
+        solarSystem.DisplaySolarSystem();
     }
 }
