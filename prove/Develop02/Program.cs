9c603150ee06e37bde249fa123e9d7500131cfe1
Tyler Muir <tylermuir42@gmail.com>
2025-11-06 09:56:37 -0700

    main loop working

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -6,21 +6,33 @@ class Program
     {
         Menu journalMenu = new Menu();
         int userSelection;
-        journalMenu.ProcessMenu();
-        userSelection = journalMenu.ProcessMenu();
 
-        switch(userSelection)
+        bool done = false;
+
+        do
         {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-        }
+            userSelection = journalMenu.ProcessMenu();
+
+            switch (userSelection)
+            {
+                case 1:
+                //Create a new Entry object
+                //Call create on that object
+                //Add the entry to the journal
+                    break;
+                case 2:
+                //Call journal.Display()
+                    break;
+                case 3:
+                //Save to a file
+                    break;
+                case 4:
+                //Read from a file
+                    break;
+                case 5:
+                    done = true;
+                    break;
+            }
+        } while (!done);
     }
 }
