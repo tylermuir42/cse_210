0a318dd7ba000f2a648a171d5e6fef6cfa4d823f
Tyler Muir <tylermuir42@gmail.com>
2025-11-13 09:20:27 -0700

    started word class

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,17 @@
+class Word
+{
+    private string _word;
+    private bool _isHidden;
+
+    public Word(string word)
+    {
+        _word = word;
+        _isHidden = false;
+    }
+
+    public void DisplayWord()
+    {
+        Console.WriteLine(_word);
+    }
+    
+}
