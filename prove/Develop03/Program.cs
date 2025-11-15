04339d08ddb04f52162d2b830f631da57cb8a5c5
Tyler Muir <tylermuir42@gmail.com>
2025-11-14 17:53:55 -0700

    Got scripture mostly working

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,24 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Word myWord = new Word("Uncle");
-        Console.WriteLine(myWord.GetWordString());
-        myWord.DisplayWord();
-        myWord.HideWord();
-        if (myWord.IsHidden())
+        Scripture scripture = new Scripture("John", 3, 16, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+
+        while (true)
         {
-            Console.WriteLine("Word is hidden");
+            Console.Clear();
+            scripture.ShowScripture();
+
+            if (!scripture.HideWords())
+            {
+                Console.WriteLine("\nAll words are hidden. Press enter to quit.");
+                Console.ReadLine();
+                break;
+            }
+
+            Console.WriteLine("\nPress enter to hide more words, or press q to quit.");
+            string input = Console.ReadLine();
+
+            if (input.Trim().ToLower() == "q")
+            {
+                break;
+            }
+            
         }
-
-        Console.WriteLine(myWord.GetWordString());
-        myWord.DisplayWord();
-
-        var reference1 = new Reference("John", 3, 16);
-        Console.WriteLine(reference1.GetScriptureReference());
-        reference1.ShowScriptureReference();
-
-        var reference2 = new Reference("Proverbs", 3, 5, 6);
-        Console.WriteLine(reference2.GetScriptureReference());
-        reference2.ShowScriptureReference();
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -3,43 +3,109 @@ class Scripture
     private List<Word> _words = new List<Word>();
     private Reference _reference;
 
+    private string _bookName;
+    private int _chapter;
+    private int _verse;
+    private int _startVerse;
+    private int _endVerse;
+    private string _text;
+
     public Scripture(string name, int chapter, int verse, string text)
     {
+        _bookName = name;
+        _chapter = chapter;
+        _verse = verse;
+        _text = text;
 
+        ConvertToWords(_text);
     }
 
     public Scripture(string name, int chapter, int startVerse, int endVerse, string text)
     {
+        _bookName = name;
+        _chapter = chapter;
+        _startVerse = startVerse;
+        _endVerse = endVerse;
+        _text = text;
 
+        ConvertToWords(_text);
     }
 
-    public Scripture(Reference _reference, string text)
+    public Scripture(Reference reference, string text)
     {
+        _reference = reference;
+        _text = text;
 
-    }
-
-    public bool HideWords()
-    {
-        return _words.Count > 0;
+        ConvertToWords(_text);
     }
 
     public void ShowScripture()
     {
-
+        Console.WriteLine(GetScripture());
     }
 
-    public string GetScriptureReference()
+    public string GetScripture()
     {
-        return "";
+        return GetScriptureString();
     }
 
-    private int NumberOfHiddenWords()
+    private string GetScriptureString()
     {
-        return 0;
+        string referenceLine;
+
+        if (_startVerse == 0 && _endVerse == 0)
+        {
+            referenceLine =  $"{_bookName} {_chapter}: {_verse} \n {_text}";
+        }
+        else
+        {
+            referenceLine = $"{_bookName} {_chapter}: {_startVerse}-{_endVerse} \n {_text}";
+        }
+
+        string combined = "";
+
+        foreach (Word w in _words)
+        {
+            combined += w.ToString() + " ";
+        }
+
+        return referenceLine + combined.TrimEnd();
     }
+
+    public bool HideWords()
+    {
+        Random rand = new Random();
+        int wordsToHide = 3;
+
+        var visibleWords = _words.Where(_words => !_words.IsHidden()).ToList();
+
+        if (visibleWords.Count == 0)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < wordsToHide && visibleWords.Count > 0; i++)
+        {
+            int index = rand.Next(visibleWords.Count);
+            visibleWords[index].HideWord();
+            visibleWords.RemoveAt(index);
+        }
+
+        return true;
+    }
+
+    // private int NumberOfHiddenWords()
+    // {
+    //     return 3;
+    // }
     
-    private string ConvertToWords(string text)
+    private void ConvertToWords(string _text)
     {
-        return "";
+        string[] parts = _text.Split(' ');
+
+        foreach (string part in parts)
+        {
+            _words.Add(new Word(part));
+        }
     }
 }
