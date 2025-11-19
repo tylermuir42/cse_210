a4d03e0cdd7457f0cde445da5fb708daec8d9031
Tyler Muir <tylermuir42@gmail.com>
2025-11-19 09:53:32 -0700

    Animation program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        string animationString = "\\|/-";
+        int sleepTime = 250;
+        int duration = 9;
+        int index = 0;
+        DateTime currentTime = DateTime.Now;
+        DateTime endTime = currentTime.AddSeconds(duration);
+
+        // while(DateTime.Now < endTime)
+        // {
+        //     Console.Write(animationString[index++ % animationString.Length]);
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b");
+        // }
+
+        int count = duration;
+        while(DateTime.Now < endTime)
+        {
+            Console.Write(count--);
+            Thread.Sleep(1000);
+            if(count >= 9)
+            {
+                Console.Write("b\b. \b\b");
+            }
+            else
+            {
+                Console.Write("\b");
+            }
+            Console.Write("\b");
+        }
     }
 }
