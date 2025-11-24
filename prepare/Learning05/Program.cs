4c99d153a059625d67c5bd7c1d918d22069f6b14
Tyler Muir <tylermuir42@gmail.com>
2025-11-24 12:55:23 -0700

    Completed Learning05 Shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 4.0);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Blue", 3.0, 5.0);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Green", 2.5);
+        shapes.Add(s3);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
