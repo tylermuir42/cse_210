8c5c0f55dd4cbd1f80193e5a73aa7c9f350b2e51
Tyler Muir <tylermuir42@gmail.com>
2025-12-02 09:42:46 -0700

    Added classes to base goal

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -16,6 +16,50 @@ class Goal
         _goalType = "";
     }
 
+    public Goal()
+    {
+        _name = "";
+        _description = "";
+        _numberOfPoints = 0;
+        _status = false; // Default status is incomplete
+        _goalType = "";
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    public void SetDescription(string description)
+    {
+        _description = description;
+    }
+
+    public int GetNumberOfPoints()
+    {
+        return _numberOfPoints;
+    }
+
+    public void SetNumberOfPoints(int points)
+    {
+        _numberOfPoints = points;
+    }
+
+    public bool GetStatus()
+    {
+        return _status;
+    }
+
+    public void SetStatus(bool status)
+    {
+        _status = status;
+    }
+
     public virtual string GetConsoleString()
     {
         return $"Name: {_name}, Description: {_description}, Points: {_numberOfPoints}, Status: {_status}";
